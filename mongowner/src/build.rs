961b5b84cd884f041dc0e97081b28323b08a4a2b
Naafiyan Ahmed <naafiyan@gmail.com>
2023-11-04 17:08:46 -0400

    added basic buildscript file io

--- mongowner/src/build.rs
+++ mongowner/src/build.rs
@@ -1,9 +1,23 @@
-use std::env;
-use std::fs;
 use quote::quote;
-
+use std::fs::File;
+use std::io::prelude::*;
+use std::path::Path;
 fn main() {
-    println!("Hello buildscript");
+    // TODO: ensure that this script is run EVERYTIME a Schema object changes
+    let path = Path::new("src/hello_world.txt");
+    let display = path.display();
+    // Open the path in read-only mode, returns `io::Result<File>`
+    let mut file = match File::open(&path) {
+        Err(why) => panic!("couldn't open {}: {}", display, why),
+        Ok(file) => file,
+    };
+
+    // Read the file contents into a string, returns `io::Result<usize>`
+    let mut s = String::new();
+    match file.read_to_string(&mut s) {
+        Err(why) => panic!("couldn't read {}: {}", display, why),
+        Ok(_) => print!("{} contains:\n{}", display, s),
+    }
 }
 
 
