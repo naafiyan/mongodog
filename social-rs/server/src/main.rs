4195558d8ece5d4350cd9cd2e5bfc072a6fe1e53
Naafiyan Ahmed <naafiyan@gmail.com>
2023-11-04 20:32:42 -0400

    updated to handle actix server

--- social-rs/server/src/main.rs
+++ social-rs/server/src/main.rs
@@ -13,7 +13,7 @@ use uuid::Uuid;
 use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
 
 const DB_NAME: &str = "social";
-const COLL_NAME: &str = "users"; // TODO: N - remove - just a placeholder for now
+
 #[get("/")]
 async fn home() -> impl Responder {
     HttpResponse::Ok().body("Welcome to social_rs")
@@ -23,12 +23,12 @@ async fn home() -> impl Responder {
 async fn get_all_users() -> impl Responder {
     HttpResponse::Ok().body("Getting all users:")
 }
-///
+
 /// Gets the user with the supplied username.
 #[get("/get_user/{username}")]
 async fn get_user(client: web::Data<Client>, username: web::Path<String>) -> HttpResponse {
     let username = username.into_inner();
-    let collection: Collection<User> = client.database(DB_NAME).collection(COLL_NAME);
+    let collection: Collection<User> = client.database(DB_NAME).collection(User::collection_name());
     match collection
         .find_one(doc! { "username": &username }, None)
         .await
@@ -69,47 +69,21 @@ async fn add_user(client: web::Data<Client>, form: web::Form<User>) -> HttpRespo
 // }
 //
 // helper function to connect to MongoDB
-async fn mongo_connect(uri: String) -> Result<(Database)> {
-    let client_options = ClientOptions::parse(&uri).await?;
-    let client = Client::with_options(client_options)?;
-    Ok(client.database(DB_NAME))
-}
 
-#[tokio::main]
-async fn main() -> Result<()> {
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
     dotenv().ok();
     // Replace the placeholder with your Atlas connection string
-    let db = {
-        let mongo_uri = std::env::var("MONGOURI")
-            .expect("MONGOURI field must be set");
-        mongo_connect(mongo_uri).await?
-    };
+    let uri = std::env::var("MONGOURI").unwrap_or_else(|_| "mongodb://localhost:27017".into());
+    let client = Client::with_uri_str(uri).await.expect("failed to connect");
 
-    let test_user_a = User {
-        user_id: Uuid::new_v4(),
-        username: "Alice".to_string(),
-        first_name: "Alice".to_string(),
-        last_name: "Bob".to_string(),
-        age: 2,
-        email: "alice_bob".to_string(),
-    };
-
-    let post = Post {
-        text: "Hello this is a post".to_string(),
-        posted_by: test_user_a.user_id,
-        date: "12th March 2023".to_string(),
-    };
-
-    println!("Posts collection name: {:?}", Post::collection_name());
-
-    // enforces that the repository i.e. collection is of type User
-    let collection : Collection<User> = db.collection(User::collection_name());
-    collection.insert_one(&test_user_a, None).await?;
-
-    let found_user = collection
-        .find_one(doc! { "username": "Alice" }, None)
-        .await?
-        .unwrap();
-
-    Ok(())
+    HttpServer::new(move || {
+        App::new()
+            .app_data(web::Data::new(client.clone()))
+            .service(add_user)
+            .service(get_user)
+    })
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
 }
